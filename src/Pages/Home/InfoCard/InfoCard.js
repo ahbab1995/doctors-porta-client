b6c7389898de4daa91723a703f7025c5ba811adf
ahbab1995 <islamahbab1996@yahoo.com>
2023-06-10 23:22:11 +0600

    info card dynamic data loud new code added

--- src/Pages/Home/InfoCard/InfoCard.js
+++ src/Pages/Home/InfoCard/InfoCard.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-const InfoCard = ({img}) => {
+const InfoCard = ({card}) => {
+    const {name,description,icon,bgClass} = card;
   return (
     <div>
-      <div className="card text-white p-6 md:card-side bg-base-100 shadow-xl bg-accent">
+      <div className={`card text-white p-6 md:card-side bg-base-100 shadow-xl ${bgClass}`}>
         <figure>
           <img
-            src={img}
+            src={icon}
             alt="Album"
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Opening Hours</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
+          <h2 className="card-title">{name}</h2>
+          <p className="leading-4">{description}</p>
           
         </div>
       </div>
--- src/Pages/Home/InfoCard/InfoCards.js
+++ src/Pages/Home/InfoCard/InfoCards.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import InfoCard from './InfoCard';
-import clock from '../../../assets/icons/clock.svg'
+import React from "react";
+import InfoCard from "./InfoCard";
+import clock from "../../../assets/icons/clock.svg";
+import marker from '../../../assets/icons/marker.svg';
+import phone from '../../../assets/icons/phone.svg';
 const InfoCards = () => {
-    return (
-        <div className='grid mt-8 gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
-        </div>
-    );
+  const cardData = [
+    {
+      id: 1,
+      name: "Opening Hours",
+      description: "Open 9.00 am to 5.00pm everyday",
+      icon: clock,
+      bgClass: "bg-gradient-to-r from-primary to-secondary",
+    },
+    {
+      id: 2,
+      name: "Visit our location",
+      description: "sylhet,Bangladesh",
+      icon: marker,
+      bgClass: "bg-accent",
+    },
+    {
+      id: 3,
+      name: "Contact us now",
+      description: "3.40",
+      icon: phone,
+      bgClass: "bg-gradient-to-r from-primary to-secondary",
+    },
+  ];
+  return (
+    <div className="grid mt-8 gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {cardData.map((card) => (
+        <InfoCard key={card.id} card={card}></InfoCard>
+      ))}
+    </div>
+  );
 };
 
 export default InfoCards;
