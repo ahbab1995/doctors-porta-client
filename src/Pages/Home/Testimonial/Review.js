07202a8c86bc9abbad977f7d27e19032e80700a5
ahbab1995 <islamahbab1996@yahoo.com>
2023-06-11 21:23:37 +0600

    Testimonial and review components new code added

--- src/Pages/Home/Testimonial/Review.js
+++ src/Pages/Home/Testimonial/Review.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const Review = () => {
-    return (
-        <div>
-            
+const Review = ({ review }) => {
+  const { name, img, location, text } = review;
+  return (
+    <div className="card w-96  shadow-xl">
+      <div className="card-body">
+        <p>{text}</p>
+        <div className=" ">
+          <div className="flex items-center mt-6">
+            <div className="avatar mr-6">
+              <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img src={img} />
+              </div>
+            </div>
+            <div>
+              <h5 className="text-lg">{name}</h5>
+              <p>{location}</p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Review;
--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -3,27 +3,28 @@ import quote from '../../../assets/icons/quote.svg'
 import people1 from '../../../assets/images/people1.png'
 import people2 from '../../../assets/images/people2.png'
 import people3 from '../../../assets/images/people3.png'
+import Review from './Review';
 const Testimonial = () => {
     const reviews = [
         {
             _id: 1, 
             name: 'Winson Herry',
             img: people1,
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            text: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
             location: 'California'
         },
         {
             _id: 2, 
             name: 'Winson Herry',
             img: people2,
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            text: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
             location: 'California'
         },
         {
             _id: 3, 
             name: 'Winson Herry',
             img: people3,
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            text: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
             location: 'California'
         },
     ]
@@ -39,8 +40,14 @@ const Testimonial = () => {
                 <img className='w-24 lg:w-48' src={quote} alt="" />
                 </div>
             </div>
-            <div>
 
+            <div className='mt-24 lg:mt-20 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center'>
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
             </div>
         </section>
     );
