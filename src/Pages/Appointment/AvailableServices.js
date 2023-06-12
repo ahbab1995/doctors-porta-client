757b4d16aaf2c64c777a16c79f8703b8cbe90e12
ahbab1995 <islamahbab1996@yahoo.com>
2023-06-12 22:57:32 +0600

    available service componinct new code added

--- src/Pages/Appointment/AvailableServices.js
+++ src/Pages/Appointment/AvailableServices.js
@@ -1,7 +1,14 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useState,useEffect } from "react";
+
 
 const AvailableServices = ({selected}) => {
+    const [services,setServices] = useState([])
+    useEffect(() => {
+       fetch('services.json')
+       .then(res=>res.json())
+       .then(data =>setServices(data))
+    }, []);
   return (
     <div>
       <p className="text-center text-secondary font-bold">
